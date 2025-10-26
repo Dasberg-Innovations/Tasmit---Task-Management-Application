a60902581851d1c51010a6cd8116b36b76ce7b92
BabyBlob <robloxdog@outlook.com>
2025-10-26 18:52:24 -0400

    Importation of User Schema Model in Index.js for usage.

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { PORT, MongoDbUrl } from './config.js';
 import mongoose, { mongo } from 'mongoose';
-
+import { User } from './Models/UserLoginModel.js';
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', (request, response) =>{
     console.log(request)
     return response.status(234).send('Testing')
 });
@@ -16,10 +16,10 @@ mongoose
     .then(() => {
         console.log('MongoDB Database is successfully connected');
         app.listen(PORT, () => {
-            console.log(`Server is being hosted on localhost: ${PORT}`);
-        });
+    console.log(`Server is being hosted on localhost: ${PORT}`);
+});
 
     })
-    .catch((error) => {
+    .catch((error) =>{
         console.log(error);
     });
