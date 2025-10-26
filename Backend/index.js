0cba9bf3d8fff16356c983375c1635c4838688ef
BabyBlob <robloxdog@outlook.com>
2025-10-26 16:57:34 -0400

    Implementation of Database Connection in Index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,25 @@
 import express from 'express'
-import { PORT } from './config.js';
+import { PORT, MongoDbUrl } from './config.js';
+import mongoose, { mongo } from 'mongoose';
 
 
 const app = express();
 
-app.get('/', (request, response) =>{
+app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Testing')
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is being hosted on localhost: ${PORT}`);
-});
+
+mongoose
+    .connect(MongoDbUrl)
+    .then(() => {
+        console.log('MongoDB Database is successfully connected');
+        app.listen(PORT, () => {
+            console.log(`Server is being hosted on localhost: ${PORT}`);
+        });
+
+    })
+    .catch((error) => {
+        console.log(error);
+    });
