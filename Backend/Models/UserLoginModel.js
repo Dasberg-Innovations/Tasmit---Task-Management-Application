ad9151621aeebc8eca4751aca6fb156e3492df4c
BabyBlob <robloxdog@outlook.com>
2025-10-26 20:03:23 -0400

    Removal of Roles - User and Admin Roles wouldn't be needed as there would no longer be a need for Admin role on the app.

--- Backend/Models/UserLoginModel.js
+++ Backend/Models/UserLoginModel.js
@@ -9,7 +9,6 @@ const UserSchema = mongoose.Schema(
             trim: true,
             sparse: true
         },
-
         email: {
             type: String,
             required: false,
@@ -19,41 +18,23 @@ const UserSchema = mongoose.Schema(
             sparse: true,
             match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a Valid Email Address.']
         },
-
         password: {
             type: String,
             required: true
         },
-
-        role: {
-            type: String,
-            enum: ['User', 'Admin'],
-            default: 'User'
-        }
     },
-
     {
         timestamps: true
     }
 );
 
-UserSchema.pre('validate', function (next) {
-    if (this.identifierType === 'email') {
-        const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-        if (!emailRegex.test(this.identifier)) {
-            this.invalidate('identifier', 'Please enter a valid email address');
-        }
-    }
-    next();
-});
-
 UserSchema.pre('save', function (next) {
-    if (this.identifier.toLowerCase() === 'admin' ||
-        this.identifier.toLowerCase() === 'admin@example.com') {
+    const identifier = this.username || this.email; 
+
+    if (identifier && (identifier.toLowerCase() === 'admin' || identifier.toLowerCase() === 'admin@example.com')) {
         this.role = 'Admin';
     }
     next();
 });
 
 export const User = mongoose.model('User', UserSchema, 'User');
-export default User;
