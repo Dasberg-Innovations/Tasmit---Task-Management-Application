0e43f8e93804a7e2d8808fe43c0738f9f9cf79c7
BabyBlob <robloxdog@outlook.com>
2025-11-24 16:01:06 -0400

    Get Task by ID + Update Task

--- Backend/Controllers/taskController.js
+++ Backend/Controllers/taskController.js
@@ -23,3 +23,35 @@ const createTask = async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 };
+
+const getTask = async (req, res, next) => {
+    try {
+        const task = await Task.findById(req.params.id).populate('username');
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+        res.task = task;
+        next();
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
+
+
+const updateTask = async (req, res) => {
+    if (req.body.Task_Title != null) {
+        res.task.Task_Title = req.body.Task_Title;
+    }
+    if (req.body.Description != null) {
+        res.task.Description = req.body.Description;
+    }
+    if (req.body.Task_Completed != null) {
+        res.task.Task_Completed = req.body.Task_Completed;
+    }
+    try {
+        const updatedTask = await res.task.save();
+        res.json(updatedTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
